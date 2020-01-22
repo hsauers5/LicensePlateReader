38e99a6c6a09a94e88a41b98aeee60253747d34d
hsauers5@gmail.com <hsauers5@gmail.com>
2020-01-22 14:07:43 -0500

    examples

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import cv2
+import sys
+from openalpr import Alpr
+
+alpr = Alpr("us", "/path/to/openalpr.conf", "/path/to/runtime_data")
+if not alpr.is_loaded():
+    print("Error loading OpenALPR")
+    sys.exit(1)
+
+alpr.set_top_n(20)
+alpr.set_default_region("md")
+
+video = cv2.VideoCapture()
+
+results = alpr.recognize_ndarray(video)
+i = 0
+for plate in results['results']:
+    i += 1
+    print("Plate #%d" % i)
+    print("   %12s %12s" % ("Plate", "Confidence"))
+    for candidate in plate['candidates']:
+        prefix = "-"
+        if candidate['matches_template']:
+            prefix = "*"
+
+        print("  %s %12s%12f" % (prefix, candidate['plate'], candidate['confidence']))
+
+# Call when completely done to release memory
+alpr.unload()
